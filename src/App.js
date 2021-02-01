63eeb63cbf9ff1b6fa08dd09e3ab49c787dec1d9
santoshdevjadhav <santoshdevjadhav@gmail.com>
2021-02-01 15:55:26 +0530

    update the functionality of sorting

--- src/App.js
+++ src/App.js
@@ -35,28 +35,49 @@ const useStyles = makeStyles(() => ({
 
 function App(props) {
   const classes = useStyles(props);
-  const [state, setState] = useState({ filterList: data });
-  const { filterList } = state;
-  function sortByUpVotes(sortBy = "desc") {
+  const [state, setState] = useState({
+    filterList: null,
+    sortUpVotes: "desc",
+    sortDate: "desc",
+  });
+  const { filterList, sortUpVotes, sortDate } = state;
+
+  if (filterList === null) {
+    sortByUpVotes();
+  }
+
+  function sortByUpVotes(sortUpVotes = "desc") {
     let list = [];
-    if (sortBy === "desc")
+    if (sortUpVotes === "desc")
       list = data.sort((a, b) => parseFloat(b.upVotes) - parseFloat(a.upVotes));
+    //sort by upvotes in descending order
     else
-      list = data.sort((a, b) => parseFloat(a.upVotes) - parseFloat(b.upVotes));
+      list = data.sort((a, b) => parseFloat(a.upVotes) - parseFloat(b.upVotes)); //sort by upvotes in ascending order
     setState((prevState) => ({ ...prevState, filterList: list }));
   }
 
-  function sortByDate(sortBy = "desc") {
+  function sortByDate() {
     let list = [];
-    if (sortBy === "desc")
+    if (sortDate === "desc")
       list = data.sort(function (a, b) {
-        return new Date(a.date) - new Date(b.date);
+        //sort by date in descending order
+        return new Date(b.date) - new Date(a.date);
       });
     else
       list = data.sort(function (a, b) {
-        return new Date(b.date) - new Date(a.date);
+        return new Date(a.date) - new Date(b.date); //sort by date in ascending order
       });
     setState((prevState) => ({ ...prevState, filterList: list }));
+  }
+  function formatDate(date) {
+    // function for date format 'yy-mm-dd'
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    return [year, month, day].join("-");
   }
 
   return (
@@ -65,27 +86,39 @@ function App(props) {
         <Grid item xs={12}>
           <Paper elevation={4}>
             <Grid container justify="flex-end">
-              <Grid item xs={12} sm={8} md={8} lg={10}>
+              <Grid item xs={12} sm={8} md={8} lg={8}>
                 <Typography className={classes.heading}>
-                  Sorting Articles
+                  Article List
                 </Typography>
               </Grid>
-              <Grid item sm={2} xs={6} md={2} lg={1}>
+              <Grid item sm={2} xs={6} md={2} lg={2}>
                 <Button
-                  onClick={() => sortByUpVotes()}
+                  onClick={() => {
+                    setState((prevState) => ({
+                      ...prevState,
+                      sortUpVotes: sortUpVotes === "desc" ? "asc" : "desc",
+                    }));
+                    sortByUpVotes(sortUpVotes === "desc" ? "asc" : "desc");
+                  }}
                   variant="contained"
                   className={classes.button}
                 >
-                  UpVotes
+                  {`${sortUpVotes === "desc" ? "Least " : "Most "}Upvoted`}
                 </Button>
               </Grid>
-              <Grid item xs={6} sm={2} md={2} lg={1}>
+              <Grid item xs={6} sm={2} md={2} lg={2}>
                 <Button
-                  onClick={() => sortByDate()}
+                  onClick={() => {
+                    setState((prevState) => ({
+                      ...prevState,
+                      sortDate: sortDate === "desc" ? "asc" : "desc",
+                    }));
+                    sortByDate();
+                  }}
                   variant="contained"
                   className={classes.button}
                 >
-                  Date
+                  {`${sortDate === "desc" ? "Most " : "Least "}Recent`}
                 </Button>
               </Grid>
             </Grid>
@@ -103,32 +136,26 @@ function App(props) {
           </Grid>
         </Grid>
         <div className={classes.list}>
-          {filterList.map((item, index) => (
-            <Grid container key={index}>
-              <Grid item xs={4}>
-                <Typography className={classes.listItem}>
-                  {item.title}
-                </Typography>
+          {filterList &&
+            filterList.map((item, index) => (
+              <Grid container key={index}>
+                <Grid item xs={4}>
+                  <Typography className={classes.listItem}>
+                    {item.title}
+                  </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography className={classes.listItem}>
+                    {item.upVotes}
+                  </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography className={classes.listItem}>
+                    {formatDate(item.date)}
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={4}>
-                <Typography className={classes.listItem}>
-                  {item.upVotes}
-                </Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography className={classes.listItem}>
-                  &nbsp; &nbsp; &nbsp; &nbsp;
-                  {new Date(item.date).toLocaleDateString()}
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  {new Date(item.date).toLocaleTimeString("en-US", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: true,
-                  })}
-                </Typography>
-              </Grid>
-            </Grid>
-          ))}
+            ))}
         </div>
       </Grid>
     </div>
